81c3e77954ba4763f60926cd20b10ba6977d0ecf
Germán Almaraz <germanalmaraz@hotmail.com>
2018-05-30 15:40:14 -0500

    Draw plays into board

--- Test.rb
+++ Test.rb
@@ -1,57 +1,48 @@
 
 class Prepare_Board
 
-  def nine_nine
-    i = 0
-    j = 0
-    $i_turn = 0
-    $casillas = Hash.new(0)
-    num = 0
+  def initialize
+    @boxes = {1 => "   ", 2 => "   ", 3 => "   ", 4 => "   ",
+                  5 => "   ", 6 => "   ", 7 => "   ", 8 => "   ", 9 => "   ", }
+    @turn = " X "
 
-    $block = { :empty => "   ", :to_r => "║   ", :mid => "═══",
-            :mid_to_r => "╬═══" }
-
-    while i<5
-      if $i_turn==0
-        j = 0
-        while j<3
-          num += 1
-          $casillas[num] = " #{num} "
-          j += 1
-        end
-        print $casillas[num-2], "║", $casillas[num-1], "║", $casillas[num], "\n"
-        $i_turn = 1
-        else
-          print $block[:mid], $block[:mid_to_r], $block[:mid_to_r], "\n"
-          $i_turn=0
-      end
-    i += 1
   end
-end
 
-  def play(casilla_seleccionada)
-    k = 0 
-    $serie_casilla = 0 
-    print $casillas[casilla_seleccionada], "\n"
-    $casillas[casilla_seleccionada] = " X "
-    print $casillas[2], "\n"
-    print $casillas[casilla_seleccionada], "\n"
-    while k<5
-      if $i_turn==1
-        $serie_casilla += 3
-        print $casillas[$serie_casilla-2], "║", $casillas[$serie_casilla-1], "║", $casillas[$serie_casilla], "\n"
-        $i_turn = 0
+  def drawBoard
+
+    box = 0
+    boardRow = 0
+
+    for i in 0..4
+
+      if boardRow == 0
+         print @boxes[box+1], "║", @boxes[box+2], "║", @boxes[box+3], "\n"
+         box +=3
+         boardRow = 1
       else
-        print $block[:mid], $block[:mid_to_r], $block[:mid_to_r], "\n"
-        $i_turn=1
+         print "═══", "╬═══", "╬═══", "\n"
+         boardRow = 0
       end
-    k += 1
+
     end
+
+  end
+
+  def play(selected_box)
+
+    @boxes[selected_box.to_i] = @turn
+    drawBoard
+
   end
 
 end
 
 main = Prepare_Board.new
 
-main.nine_nine
-main.play(gets)
+main.drawBoard
+
+for i in 0..8
+
+  main.play(gets)
+
+end
